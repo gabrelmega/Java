0e2e0d2e8d4b0bf50d475cf18a699f19c80638bd
Mega <gabrelmega123123@gmail.com>
2024-07-05 17:02:45 -0300

    FINISHED PROJECT Now the playRound() function is INSIDE the playGame() function. New score system. With the variables humanScore and computerScore. Now the game repeats 5 times, using the for() loop. Now each conditional statement inside playRound() function gives a score. The game gives a result depending of who wons.

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 console.log("Hello World")
 
-let humanScore = 0;
-let computerScore = 0;
-
-//second way that you can do it
 function getComputerChoice(max) {
     const choice = Math.floor(Math.random() * max);
     if (choice === 0) {
@@ -22,35 +18,54 @@ function getHumanChoice() {
 }
 
 //Let's play!!!
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toUpperCase() === computerChoice) {
-        console.log("It's a tie! Both choose the same item.");
-    } else if (humanChoice.toUpperCase() === "ROCK") {
-        if (computerChoice === "SCISSORS") {
-            console.log("You won this round! Rock beats scissors.");
-        } else if (computerChoice === "PAPER") {
-            console.log("You lose this round. Paper beats rock.");
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice.toUpperCase() === computerChoice) {
+            console.log("It's a tie! Both choose the same item.");
+        } else if (humanChoice.toUpperCase() === "ROCK") {
+            if (computerChoice === "SCISSORS") {
+                console.log("You won this round! Rock beats scissors.");
+                humanScore += 1
+            } else if (computerChoice === "PAPER") {
+                console.log("You lose this round. Paper beats rock.");
+                computerScore += 1
+            }
+        } else if (humanChoice.toUpperCase() === "PAPER") {
+            if (computerChoice === "ROCK") {
+                console.log("You won this round! Paper beats rock.");
+                humanScore += 1
+            } else if (computerChoice === "SCISSORS") {
+                console.log("You lose this round. Scissors beats paper.");
+                computerScore += 1
+            }
+        } else if (humanChoice.toUpperCase() === "SCISSORS") {
+            if (computerChoice === "ROCK") {
+                console.log("You lose this round! Rock beats scissors.");
+                computerScore += 1
+            } else if (computerChoice === "PAPER") {
+                console.log("You won this round! Scissors beats paper.");
+                humanScore += 1
+            }
+        } else {
+            alert("You need to choose: Rock, Paper or Scissors.");
+            playRound(getHumanChoice(), getComputerChoice());
         }
-    } else if (humanChoice.toUpperCase() === "PAPER") {
-        if (computerChoice === "ROCK") {
-            console.log("You won this round! Paper beats rock.");
-        } else if (computerChoice === "SCISSORS") {
-            console.log("You lose this round. Scissors beats paper.");
-        }
-    } else if (humanChoice.toUpperCase() === "SCISSORS") {
-        if (computerChoice === "ROCK") {
-            console.log("You lose this round! Rock beats scissors.");
-        } else if (computerChoice === "PAPER") {
-            console.log("You won this round! Scissors beats paper.");
-        }
+    }
+    for (let i = 0; i<5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSlelection = getComputerChoice(3)
+        playRound (humanSelection, computerSlelection)
+    }
+
+    if (humanScore > computerScore) {
+        console.log("YOU WIN!!!")
+    } else if (computerScore > humanScore) {
+        console.log("YOU LOSE.")
     } else {
-        alert("You need to choose: Rock, Paper or Scissors.");
+        console.log("It's a tie!")
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice(3);
-
-playRound(humanSelection, computerSelection);
-
-console.log(computerSelection);
+playGame()
