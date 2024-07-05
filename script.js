4b21011cd315d66ec7fbbf053a744273696ab726
Mega <gabrelmega123123@gmail.com>
2024-07-05 13:02:08 -0300

    Added new getHumanChoice function. First letter capitalized or not. It will detect the choice.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Hello World")
 
 //First way that i did it
 function getComputerChoice(max) {
-    let choice = Math.floor(Math.random() * max)
+    const choice = Math.floor(Math.random() * max)
     if (choice === 0) {
         return "Rock";
     } else if (choice === 1) {
@@ -16,7 +16,7 @@ console.log(getComputerChoice(3))
 
 //second way that you can do it
 function getComputerChoiceTwo() {
-    let choice = Math.random() * 10;
+    const choice = Math.random() * 10;
     if (choice <= 3) {
         return "Rock";
     } else if (choice > 3 && choice <= 6) {
@@ -27,3 +27,19 @@ function getComputerChoiceTwo() {
 }
 
 console.log(getComputerChoiceTwo());
+
+//getting the human choice
+function getHumanChoice() {
+    const choice = prompt("Your choice")
+    if (choice == "rock" || choice == "Rock") {
+        return "Rock";
+    } else if (choice == "paper" || choice == "Paper") {
+        return "Paper";
+    } else if (choice == "scissors" || choice == "Scissors") {
+        return "Scissors";
+    } else {
+        alert("You need to choose Rock, Paper or Scissors.");
+        getHumanChoice();
+    }
+}
+console.log(getHumanChoice())
