ed933daf6fecc3b4c2987ed12eb8b89b9b9e27b6
Mega <gabrelmega123123@gmail.com>
2024-07-18 19:55:12 -0300

    New UI Now the game is no more playabe on prompt.

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-console.log("Hello World")
+console.log("Hello World");
+
+let playerScore = 0;
+let computerScore = 0;
+
+const btn = document.querySelectorAll("#btn");
+const roundWinner = document.querySelector("#roundWinner");
+const paraRound = document.querySelector("#winnerPara");
+const player = document.querySelector("#playerUI");
+const computer = document.querySelector("#computerUI");
+const winnerContainer = document.querySelector("#winnerContainer");
+const btnContainer = document.querySelector("#btnContainer");
 
 function getComputerChoice(max) {
     const choice = Math.floor(Math.random() * max);
@@ -11,61 +22,68 @@ function getComputerChoice(max) {
     }
 }
 
-//getting the human choice
-function getHumanChoice() {
-    const choice = prompt("Your choice");
-    return choice;
+btn.forEach((btn) => {
+    if(btn.className) {
+        btn.addEventListener("click", () => {
+            playRound(btn.className, getComputerChoice(3));
+        });
+    }
+});
+
+function gameOver() {
+    btnContainer.innerHTML = '';
 }
 
-//Let's play!!!
-function playGame() {
-    let humanScore = 0
-    let computerScore = 0
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice.toUpperCase() === computerChoice) {
-            console.log("It's a tie! Both choose the same item.");
-        } else if (humanChoice.toUpperCase() === "ROCK") {
-            if (computerChoice === "SCISSORS") {
-                console.log("You won this round! Rock beats scissors.");
-                humanScore += 1
-            } else if (computerChoice === "PAPER") {
-                console.log("You lose this round. Paper beats rock.");
-                computerScore += 1
-            }
-        } else if (humanChoice.toUpperCase() === "PAPER") {
-            if (computerChoice === "ROCK") {
-                console.log("You won this round! Paper beats rock.");
-                humanScore += 1
-            } else if (computerChoice === "SCISSORS") {
-                console.log("You lose this round. Scissors beats paper.");
-                computerScore += 1
-            }
-        } else if (humanChoice.toUpperCase() === "SCISSORS") {
-            if (computerChoice === "ROCK") {
-                console.log("You lose this round! Rock beats scissors.");
-                computerScore += 1
-            } else if (computerChoice === "PAPER") {
-                console.log("You won this round! Scissors beats paper.");
-                humanScore += 1
-            }
-        } else {
-            alert("You need to choose: Rock, Paper or Scissors.");
-            playRound(getHumanChoice(), getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        paraRound.textContent = "It's a tie! Both choose the same item.";
+    } else if (playerChoice === "ROCK") {
+        if (computerChoice === "SCISSORS") {
+            paraRound.textContent = "You won this round! Rock beats scissors.";
+            playerScore++
+            player.textContent = `Player Score ${playerScore} `;
+        } else if (computerChoice === "PAPER") {
+            paraRound.textContent = "You lose this round. Paper beats rock.";
+            computerScore++
+            computer.textContent = `Computer Score ${computerScore}`;
+        }
+    } else if (playerChoice === "PAPER") {
+        if (computerChoice === "ROCK") {
+            paraRound.textContent = "You won this round! Paper beats rock.";
+            playerScore++
+            player.textContent = `Player Score ${playerScore} `;
+        } else if (computerChoice === "SCISSORS") {
+            paraRound.textContent = "You lose this round. Scissors beats paper.";
+            computerScore++
+            computer.textContent = `Computer Score ${computerScore}`;
+        }
+    } else if (playerChoice === "SCISSORS") {
+        if (computerChoice === "ROCK") {
+            paraRound.textContent = "You lose this round! Rock beats scissors.";
+            computerScore++
+            computer.textContent = `Computer Score ${computerScore}`;
+        } else if (computerChoice === "PAPER") {
+            paraRound.textContent = "You won this round! Scissors beats paper.";
+            playerScore++
+            player.textContent = `Player Score ${playerScore} `;
         }
     }
-    for (let i = 0; i<5; i++) {
-        const humanSelection = getHumanChoice()
-        const computerSlelection = getComputerChoice(3)
-        playRound (humanSelection, computerSlelection)
-    }
-
-    if (humanScore > computerScore) {
-        console.log("YOU WIN!!!")
-    } else if (computerScore > humanScore) {
-        console.log("YOU LOSE.")
-    } else {
-        console.log("It's a tie!")
+    if (playerScore == 5) {
+        const playerWon = document.createElement("h1");
+        const message = document.createElement("p");
+        playerWon.textContent = "YOU WON!!!! CONGRATULATIONS!!!";
+        message.textContent = "You already won, so don't play it again. If you want to play again, then refresh the page... No reset button for U >:("
+        winnerContainer.appendChild(playerWon);
+        winnerContainer.appendChild(message);
+        gameOver()
+    }   else if (computerScore == 5) {
+        const computerWon = document.createElement("h1");
+        const looserMessage = document.createElement("p");
+        computerWon.textContent = "You loose. Better luck next time.";
+        looserMessage.textContent = "You lost the game. I would made a reset button. But the Assiggment didn't tell me to do. So no reset button for you >:(";
+        winnerContainer.appendChild(computerWon);
+        winnerContainer.appendChild(looserMessage);
+        gameOver()
     }
 }
 
-playGame()
